d408770807c342db8c7243221b88fe4ad10acc3d
NirashaFernando <nirasharosh@gmail.com>
2025-05-16 09:24:12 +0530

    update post form css changes

--- frontend/src/Pages/PostManagement/UpdatePost.js
+++ frontend/src/Pages/PostManagement/UpdatePost.js
@@ -14,7 +14,7 @@ function UpdatePost() {
 
   useEffect(() => {
     // Fetch the post details
-    const fetchPost = async () => {
+    const etchPost = async () => {
       try {
         const response = await axios.get(`http://localhost:8080/posts/${id}`);
         const post = response.data;
